5dfa4a7d6b6407647a4348b881301702a41fd9fa
Rohit Pandey <rohit-pandey@hcltech.com>
2025-07-10 02:38:02 +0530

    Initial setup for our frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('ToDo Backend is Running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
